50a5de7c01d68778bb03b44265f0ddc33dcdf508
JameJohn136 <jamejohn136@outlook.com>
2022-01-18 23:13:57 -0500

    Add project files.

---
+++ Space Race/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace Space_Race
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameLoop = new System.Windows.Forms.Timer(this.components);
+            this.player1ScoreLabel = new System.Windows.Forms.Label();
+            this.player2ScoreLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameLoop
+            // 
+            this.gameLoop.Enabled = true;
+            this.gameLoop.Interval = 20;
+            this.gameLoop.Tick += new System.EventHandler(this.gameLoop_Tick);
+            // 
+            // player1ScoreLabel
+            // 
+            this.player1ScoreLabel.AutoSize = true;
+            this.player1ScoreLabel.Location = new System.Drawing.Point(36, 26);
+            this.player1ScoreLabel.Name = "player1ScoreLabel";
+            this.player1ScoreLabel.Size = new System.Drawing.Size(14, 16);
+            this.player1ScoreLabel.TabIndex = 0;
+            this.player1ScoreLabel.Text = "0";
+            // 
+            // player2ScoreLabel
+            // 
+            this.player2ScoreLabel.AutoSize = true;
+            this.player2ScoreLabel.Location = new System.Drawing.Point(267, 26);
+            this.player2ScoreLabel.Name = "player2ScoreLabel";
+            this.player2ScoreLabel.Size = new System.Drawing.Size(14, 16);
+            this.player2ScoreLabel.TabIndex = 1;
+            this.player2ScoreLabel.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(332, 453);
+            this.Controls.Add(this.player2ScoreLabel);
+            this.Controls.Add(this.player1ScoreLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameLoop;
+        private System.Windows.Forms.Label player1ScoreLabel;
+        private System.Windows.Forms.Label player2ScoreLabel;
+    }
+}
+
---
+++ Space Race/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Space_Race
+{
+    public partial class Form1 : Form
+    {
+        // Players
+        Rectangle player1 = new Rectangle(70, 100000, 10, 30); // Set as a high number so they instantly go to the bottom of the screen
+        Rectangle player2 = new Rectangle(165, 100000, 10, 30);
+
+        // The 'asteroids', "dot" is the best short term I could think of
+        Rectangle dot = new Rectangle(295, 195, 6, 6);
+
+        // Player scores are saved here and default to 0
+        int player1Score = 0;
+        int player2Score = 0;
+
+        // Speed of objects in scene
+        int playerSpeed = 4;
+        int dotSpeed = 6;
+
+        // Screen Bounderies
+        int screenY = 340;
+
+        // Input check
+        bool wDown = false;
+        bool sDown = false;
+        bool upArrowDown = false;
+        bool downArrowDown = false;
+
+        // Brushes
+        SolidBrush blueBrush = new SolidBrush(Color.DodgerBlue);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Checking "KeyDOWN" Input
+            #region
+            switch (e.KeyCode) // Detect when the input is used
+            {
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+            }
+            #endregion
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            // Checking "KeyUP" Input
+            #region
+            switch (e.KeyCode) // Detect when input is let go of
+            {
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.Up:
+                    upArrowDown = false;
+                    break;
+                case Keys.Down:
+                    downArrowDown = false;
+                    break;
+            }
+            #endregion
+        }
+
+
+        private void gameLoop_Tick(object sender, EventArgs e)
+        {
+            // Player Movement
+            #region
+            if (wDown)
+            {
+                player1.Y -= playerSpeed;
+            }
+
+            if (sDown)
+            {
+                player1.Y += playerSpeed;
+            }
+
+            if (upArrowDown)
+            {
+                player2.Y -= playerSpeed;
+            }
+
+            if (downArrowDown)
+            {
+                player2.Y += playerSpeed;
+            }
+            #endregion
+
+            // Area Bounds & Player Score Management
+            #region
+            if (player1.Y < 0) 
+            {
+                // When the player reaches the TOP of the screen
+                player1.Y = 0;
+                player1Score++;
+                player1.Y = screenY;
+
+                player1ScoreLabel.Text = player1Score.ToString();
+            }
+
+            if (player2.Y < 0)
+            {
+                // When the player reaches the TOP of the screen
+                player2.Y = 0;
+                player2Score++;
+                player2.Y = screenY;
+
+                player2ScoreLabel.Text = player2Score.ToString();
+            }
+
+            if (player1.Y > screenY)
+            {
+                // When the player reaches the BOTTOM of the screen
+                player1.Y = screenY;
+            }
+
+            if (player2.Y > screenY)
+            {
+                // When the player reaches the BOTTOM of the screen
+                player2.Y = screenY;
+            }
+            #endregion
+
+            Refresh(); // Called at the end of the game loop
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(blueBrush, player1);
+            e.Graphics.FillRectangle(blueBrush, player2);
+            e.Graphics.FillRectangle(whiteBrush, dot);
+        }
+    }
+}
