5aec2dd828a64fea6bdaed6b35a5993c81803800
JameJohn136 <jamejohn136@outlook.com>
2022-01-19 09:10:29 -0500

    ye

--- Space Race/Form1.Designer.cs
+++ Space Race/Form1.Designer.cs
@@ -33,31 +33,70 @@ namespace Space_Race
             this.gameLoop = new System.Windows.Forms.Timer(this.components);
             this.player1ScoreLabel = new System.Windows.Forms.Label();
             this.player2ScoreLabel = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.winnerText = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // gameLoop
             // 
-            this.gameLoop.Enabled = true;
             this.gameLoop.Interval = 20;
             this.gameLoop.Tick += new System.EventHandler(this.gameLoop_Tick);
             // 
             // player1ScoreLabel
             // 
             this.player1ScoreLabel.AutoSize = true;
-            this.player1ScoreLabel.Location = new System.Drawing.Point(36, 26);
+            this.player1ScoreLabel.Font = new System.Drawing.Font("Pixeboy", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player1ScoreLabel.Location = new System.Drawing.Point(7, 24);
             this.player1ScoreLabel.Name = "player1ScoreLabel";
-            this.player1ScoreLabel.Size = new System.Drawing.Size(14, 16);
+            this.player1ScoreLabel.Size = new System.Drawing.Size(19, 14);
             this.player1ScoreLabel.TabIndex = 0;
             this.player1ScoreLabel.Text = "0";
             // 
             // player2ScoreLabel
             // 
             this.player2ScoreLabel.AutoSize = true;
-            this.player2ScoreLabel.Location = new System.Drawing.Point(267, 26);
+            this.player2ScoreLabel.Font = new System.Drawing.Font("Pixeboy", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player2ScoreLabel.Location = new System.Drawing.Point(301, 24);
             this.player2ScoreLabel.Name = "player2ScoreLabel";
-            this.player2ScoreLabel.Size = new System.Drawing.Size(14, 16);
+            this.player2ScoreLabel.Size = new System.Drawing.Size(19, 14);
             this.player2ScoreLabel.TabIndex = 1;
             this.player2ScoreLabel.Text = "0";
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.titleLabel.Font = new System.Drawing.Font("Pixeboy", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(3, 90);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(326, 37);
+            this.titleLabel.TabIndex = 2;
+            this.titleLabel.Text = "SPACE RACE";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.Transparent;
+            this.startButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.startButton.Font = new System.Drawing.Font("Pixeboy", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(93, 262);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(157, 75);
+            this.startButton.TabIndex = 3;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // winnerText
+            // 
+            this.winnerText.BackColor = System.Drawing.Color.Transparent;
+            this.winnerText.Font = new System.Drawing.Font("Pixeboy", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.winnerText.Location = new System.Drawing.Point(18, 195);
+            this.winnerText.Name = "winnerText";
+            this.winnerText.Size = new System.Drawing.Size(302, 47);
+            this.winnerText.TabIndex = 4;
+            this.winnerText.Text = "PlaceHolder Text for whoever wins";
+            this.winnerText.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // Form1
             // 
@@ -65,6 +104,9 @@ namespace Space_Race
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DimGray;
             this.ClientSize = new System.Drawing.Size(332, 453);
+            this.Controls.Add(this.winnerText);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.titleLabel);
             this.Controls.Add(this.player2ScoreLabel);
             this.Controls.Add(this.player1ScoreLabel);
             this.Name = "Form1";
@@ -82,6 +124,9 @@ namespace Space_Race
         private System.Windows.Forms.Timer gameLoop;
         private System.Windows.Forms.Label player1ScoreLabel;
         private System.Windows.Forms.Label player2ScoreLabel;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label winnerText;
     }
 }
 
--- Space Race/Form1.cs
+++ Space Race/Form1.cs
@@ -8,17 +8,34 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Media;
+using System.IO;
 
 namespace Space_Race
 {
     public partial class Form1 : Form
     {
+        // Global Variables
+        #region
+        // Screen the players are on
+        string screen = "title";
+
         // Players
-        Rectangle player1 = new Rectangle(70, 100000, 10, 30); // Set as a high number so they instantly go to the bottom of the screen
-        Rectangle player2 = new Rectangle(165, 100000, 10, 30);
+        Rectangle player1 = new Rectangle(70, 100000, 10, 20); // Set as a high number so they instantly go to the bottom of the screen
+        Rectangle player2 = new Rectangle(165, 100000, 10, 20);
 
         // The 'asteroids', "dot" is the best short term I could think of
-        Rectangle dot = new Rectangle(295, 195, 6, 6);
+        List<Rectangle> dots = new List<Rectangle>();
+        List<int> dotSpeeds = new List<int>(); 
+        int xSpot = 0;
+        
+        // Have the range be 4-7
+        int minDotSpeed = 2;
+        int maxDotSpeed = 4;
+
+        int dotSizeX = 5, dotSizeY = 5;
+        int spawnChance = 30;
+
+        Random randGen = new Random();
 
         // Player scores are saved here and default to 0
         int player1Score = 0;
@@ -26,7 +43,6 @@ namespace Space_Race
 
         // Speed of objects in scene
         int playerSpeed = 4;
-        int dotSpeed = 6;
 
         // Screen Bounderies
         int screenY = 340;
@@ -37,13 +53,23 @@ namespace Space_Race
         bool upArrowDown = false;
         bool downArrowDown = false;
 
+        // Sounds
+        SoundPlayer hurtSound;
+        SoundPlayer pointSound;
+
         // Brushes
         SolidBrush blueBrush = new SolidBrush(Color.DodgerBlue);
         SolidBrush whiteBrush = new SolidBrush(Color.White);
-
+        #endregion
         public Form1()
         {
             InitializeComponent();
+            gameLoop.Enabled = false;
+            winnerText.Text = "";
+            pointSound = new SoundPlayer(Properties.Resources.pickupCoin);
+            hurtSound = new SoundPlayer(Properties.Resources.explosion);
+            hurtSound.Stop();
+            pointSound.Stop();
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
@@ -124,6 +150,8 @@ namespace Space_Race
                 player1.Y = 0;
                 player1Score++;
                 player1.Y = screenY;
+                pointSound.Stop();
+                pointSound.Play();
 
                 player1ScoreLabel.Text = player1Score.ToString();
             }
@@ -134,6 +162,8 @@ namespace Space_Race
                 player2.Y = 0;
                 player2Score++;
                 player2.Y = screenY;
+                pointSound.Stop();
+                pointSound.Play();
 
                 player2ScoreLabel.Text = player2Score.ToString();
             }
@@ -151,14 +181,131 @@ namespace Space_Race
             }
             #endregion
 
+            // Move 'Dots'
+            #region
+            for (int i = 0; i < dots.Count(); i++)
+            {
+                int x = dots[i].X + dotSpeeds[i];
+
+                dots[i] = new Rectangle(x, dots[i].Y, dotSizeX, dotSizeY);
+            }
+
+            // Should a new dot be created?
+            int randNum = randGen.Next(1, 101);
+
+            // Actually make a new 'dot' if it should
+            if (randNum < spawnChance)
+            {
+                int y = randGen.Next(10, 301);
+                // Determine which side it will spawn on
+                int sideNum = randGen.Next(1, 3);
+
+                switch (sideNum)
+                {
+                    case 1: // Left
+                        dotSpeeds.Add(randGen.Next(minDotSpeed, maxDotSpeed + 1));
+                        xSpot = dotSizeX * 2;
+                        break;
+                    case 2: // Right
+                        int tempDotSpeed = randGen.Next(minDotSpeed, maxDotSpeed + 1);
+                        // Setting it to a negative number to make it move the other way
+                        dotSpeeds.Add(-tempDotSpeed);
+                        xSpot = this.Width - dotSizeX * 2;
+                        break;
+                }
+
+                dots.Add(new Rectangle(xSpot, y, dotSizeX, dotSizeY));
+            }
+
+            // See if the dot should be removed
+            for (int i = 0; i < dots.Count(); i++)
+            {
+                if (dots[i].X > this.Width - dotSizeX)
+                {
+                    dots.RemoveAt(i);
+                    dotSpeeds.RemoveAt(i);
+                }
+            }
+
+            // Check for collision with players
+            for (int i = 0; i < dots.Count(); i++)
+            {
+                if (player1.IntersectsWith(dots[i]))
+                {
+                    player1.Y = 10000; // High number to make it go to the bottom of the play area
+                    hurtSound.Stop();
+                    hurtSound.Play();
+                }
+
+                if (player2.IntersectsWith(dots[i]))
+                {
+                    player2.Y = 10000; // High number to make it go to the bottom of the play area
+                    hurtSound.Stop();
+                    hurtSound.Play();
+                }
+            }
+            #endregion
+
+            // Player Win
+            #region
+            if (player1Score >= 3)
+            {
+                EndGame("Player1");
+            } else if (player2Score >= 3)
+            {
+                EndGame("Player2");
+            }
+            #endregion
+
             Refresh(); // Called at the end of the game loop
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            StartGame();
+        }
+
+        private void StartGame()
+        {
+            // Start the game under any condition
+            titleLabel.Text = "";
+            gameLoop.Enabled = true;
+            startButton.Enabled = false;
+            startButton.Visible = false;
+            winnerText.Text = "";
+            player1Score = 0;
+            player2Score = 0;
+            player1ScoreLabel.Text = player1Score.ToString();
+            player2ScoreLabel.Text = player2Score.ToString();
+
+
+            screen = "play";
+        }
+
+        private void EndGame(string player)
+        {
+            gameLoop.Enabled = false;
+            winnerText.Text = $"{player} wins!";
+            startButton.Enabled = true;
+            startButton.Visible = true;
+            startButton.Text = "Play Again";
+            screen = "GameOver";
+            dots.Clear();
+            dotSpeeds.Clear();
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
+            // Drawing Graphics
+            #region
             e.Graphics.FillRectangle(blueBrush, player1);
             e.Graphics.FillRectangle(blueBrush, player2);
-            e.Graphics.FillRectangle(whiteBrush, dot);
+
+            for (int i = 0; i < dots.Count(); i++)
+            {
+                e.Graphics.FillRectangle(whiteBrush, dots[i]);
+            }
+            #endregion
         }
     }
 }
